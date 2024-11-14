57ec574838f3310bccfa78ccd9a3c865c8f272c4
Sofía Derrossi <sofi.derrossi@gmail.com>
2024-11-13 23:06:03 -0300

    segunda entrega

--- script.js
+++ script.js
@@ -87,3 +87,77 @@ if (( usuario === 'Roberto' ) && Number(contrasenia === 1928) ) {
         alert("Ingrese moneda válida")
     }
     
+    alert = "Viajes hasta tres personas"
+
+
+     class Viaje {
+        constructor (lugar, noches, precio, pasajeros) {
+        this.lugar = lugar;
+        this.noches = noches;
+        this.precio = precio;
+        //this.pasajeros = pasajeros
+        this.comprado = false;
+    }
+
+    vender (){
+        this.comprado = true
+    }
+}
+
+const Viajes = [];
+
+Viaje.push = new Viaje ("Madrid", "7 noches", "2550000");
+Viaje.push = new Viaje ("Mendoza", "4 noches", "800000");
+Viaje.push = new Viaje ("Ushuaia", "5 noches", "1000000");
+
+
+//const filtro = Viajes.filter ((el)=> )
+
+
+class pasajero {
+    constructor(NombreYapellido, Edad, DNI) {
+        this.NombreYapellido = NombreYapellido;
+        this.Edad = Edad;
+        this.DNI = DNI;
+    }
+}
+
+const pasajeros = [];
+
+let cantidad = (3)
+
+ do {
+    let NombreYapellido = prompt ("Ingresar nombre y apellido")
+    let Edad = prompt ("Ingrese Edad")
+    let DNI = prompt ("Ingrese DNI")
+
+    pasajero.push = new pasajero (NombreYapellido, Edad, DNI)
+    console.log(pasajero.length)
+
+ } while (pasajero.length !=cantidad);
+
+
+
+ 
+
+
+
+
+
+
+
+
+//let Viaje = [
+//    {id: 1, lugar: "Madrid", noches: "7 noches", precio : 2550000},
+//    {id: 2, lugar: "Mendoza", noches: "4 noches", precio : 800000},
+//    {id: 2, lugar: "Ushuaia", noches: "5 noches", precio : 1000000},
+//]
+
+
+
+
+//const miArray = ["marca", 3, "palabra"]
+//miArray.push("otra cosa") // esto lo agrega al final, si quiero agregar al ppio tengo que poner .unshift
+
+//console.log (miArray.length) //va a ser 4
+//console.log (miArray) //va a ser ["marca", 3, "palabra", "otra cosa"]
